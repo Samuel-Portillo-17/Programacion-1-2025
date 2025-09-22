1c3d33db87a924f92883acede8051897e5ff93ee
Samuel <samuelportillo217@gmail.com>
2025-09-21 18:46:17 -0600

    matricez con estadistica

--- primerProyecto/primerProyecto/Form1.Designer.cs
+++ primerProyecto/primerProyecto/Form1.Designer.cs
@@ -28,21 +28,106 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblArmonica = new System.Windows.Forms.Label();
+            this.lblTipica = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblSerie = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtSerie = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // lblArmonica
+            // 
+            this.lblArmonica.AutoSize = true;
+            this.lblArmonica.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArmonica.Location = new System.Drawing.Point(33, 138);
+            this.lblArmonica.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblArmonica.Name = "lblArmonica";
+            this.lblArmonica.Size = new System.Drawing.Size(73, 17);
+            this.lblArmonica.TabIndex = 11;
+            this.lblArmonica.Text = "Armonica:";
+            // 
+            // lblTipica
+            // 
+            this.lblTipica.AutoSize = true;
+            this.lblTipica.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipica.Location = new System.Drawing.Point(33, 101);
+            this.lblTipica.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTipica.Name = "lblTipica";
+            this.lblTipica.Size = new System.Drawing.Size(49, 17);
+            this.lblTipica.TabIndex = 10;
+            this.lblTipica.Text = "Tipica:";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.Location = new System.Drawing.Point(33, 72);
+            this.lblMedia.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(50, 17);
+            this.lblMedia.TabIndex = 9;
+            this.lblMedia.Text = "Media:";
+            // 
+            // lblSerie
+            // 
+            this.lblSerie.AutoSize = true;
+            this.lblSerie.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSerie.Location = new System.Drawing.Point(33, 39);
+            this.lblSerie.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblSerie.Name = "lblSerie";
+            this.lblSerie.Size = new System.Drawing.Size(45, 17);
+            this.lblSerie.TabIndex = 8;
+            this.lblSerie.Text = "Serie:";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(344, 51);
+            this.btnCalcular.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(107, 57);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtSerie
+            // 
+            this.txtSerie.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSerie.Location = new System.Drawing.Point(88, 39);
+            this.txtSerie.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSerie.Name = "txtSerie";
+            this.txtSerie.Size = new System.Drawing.Size(163, 23);
+            this.txtSerie.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(647, 300);
+            this.Controls.Add(this.lblArmonica);
+            this.Controls.Add(this.lblTipica);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.lblSerie);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtSerie);
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Hola";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblArmonica;
+        private System.Windows.Forms.Label lblTipica;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblSerie;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtSerie;
     }
 }
 
--- primerProyecto/primerProyecto/Form1.cs
+++ primerProyecto/primerProyecto/Form1.cs
@@ -16,44 +16,40 @@ namespace primerProyecto
         {
             InitializeComponent();
         }
-        String[][] etiquetas = new string[][] {
-            new string[]{"Dolar", "Pesos Mexicanos", "Quetzal", "Lempira", "Colon SV", "Cordobas", "Colon CR"}, //Monedas
-            new string[]{"Metros", "Cm", "Pulgadas","Pie", "Varas", "Yardas", "Km", "Millas"}, //Longitud
-            new string[]{"Libra", "Onza", "Gramo", "Kg", "Quintal", "Tonelada Corta"}, //Masa
-            new string[]{"Galon Us", "Litros", "Pinta Us", "Ml"}, //Volumen
-            new string[]{"GB", "Bit", "Byte", "KB", "MG", "TB"}, //Almacenamiento
-            new string[]{"Dia", "Segundos", "Minutos", "Horas", "Semana", "Meses", "Año"}, //Tiempo
-             new string[]{"Km2", "Mt2", "Milla cuadrada", "Yarda Cuadrada", "Pie cuadrado", "Pulgada Cuadrada", "Hectarea"} //Area
-        };
-        double[][] valores = new double[][] {
-            new double []{1,18.78, 7.66, 26.15, 8.75, 36.78, 504.12}, //Monedas
-            new double []{1, 100, 39.37, 3.28084, 1.193, 1.09361, 0.001, 0.000621371}, //Longitud
-            new double []{1, 16, 453.592, 0.453592, 0.01, 0.001,0.0005}, //Masa
-            new double []{1, 3.78541, 8, 3785.41}, //Volumen
-            new double []{1, 8e+9, 1e+9, 1e+6, 1000, 0.001}, //Almacenamiento
-            new double []{1, 86400, 1440, 24, 0.142857, 0.0328767, 0.00273973}, //Tiempo
-             new double []{1, 1e+6, 0.386102, 1.196e+6, 1.076e+7, 1.55e+9, 100} //Area
-        };
 
-        private void btnConvertir_Click(object sender, EventArgs e)
+
+
+        //Matrices
+        //Funciones
+        //LINQ = Lenguaje de Consulta Integrado
+
+
+
+        double media(int[] serie)
         {
-            double cantidad = double.Parse(txtCantidadConversor.Text);
-
-            int tipo = cboTipoConversor.SelectedIndex;
-            int de = cboDeConversor.SelectedIndex;
-            int a = cboAConversor.SelectedIndex;
-
-            double respuesta = cantidad * valores[tipo][a] / valores[tipo][de];
-
-            lblRespuestaConversor.Text = "Respuesta: " + respuesta.ToString("N2");
+            return serie.Average();
+        }
+        double tipica(int[] serie, double m)
+        {
+            return Math.Sqrt(serie.Average(n => Math.Pow(n - m, 2)));
+        }
+        double armonica(int[] serie)
+        {
+            int n = serie.Length;
+            return n / serie.Sum(num => 1.0 / num);
         }
 
-        private void cboTipoConversor_SelectedIndexChanged(object sender, EventArgs e)
+        private void btnCalcular_Click(object sender, EventArgs e)
         {
-            cboDeConversor.Items.Clear();
-            cboAConversor.Items.Clear();
-            cboDeConversor.Items.AddRange(etiquetas[cboTipoConversor.SelectedIndex]);
-            cboAConversor.Items.AddRange(etiquetas[cboTipoConversor.SelectedIndex]);
+            int[] serie = txtSerie.Text.Split(',').Select(n => int.Parse(n)).ToArray(); //5,8,1,9 => ["5", "8", "1", "9"]
+
+            double mediaAritmetica = media(serie);
+            double desviacionTipica = tipica(serie, mediaAritmetica);
+            double mediaArmonica = armonica(serie);
+
+            lblMedia.Text = "Media: " + mediaAritmetica.ToString();
+            lblTipica.Text = "Tipica: " + desviacionTipica.ToString();
+            lblArmonica.Text = "Armonica: " + mediaArmonica.ToString();
         }
     }
 }
